6bf87991bded87f398ab5dd6b479511298586a09
Dralen <oginnisayofunmi12@gmail.com>
2026-04-20 17:31:45 +0100

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header"
+import AssemblyEndgame from "./components/Main"
+
+export default function App() {
+  
+  return (
+    <>
+      <Header />
+      <AssemblyEndgame />
+    </>
+  )
+}
